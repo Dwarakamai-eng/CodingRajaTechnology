9d51b8da795ce77e9ff36efe5fff7e186de0d14a
Dwarakamai-eng <150651861+Dwarakamai-eng@users.noreply.github.com>
2024-04-28 11:21:53 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,22 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PostList from './PostList';
 import PostForm from './PostForm';
 import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
 
 const App = () => {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/" component={PostList} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/create" component={PostForm} />
-          {/* Add route for editing post */}
+          <Route path="/login" render={() => (isAuthenticated ? <Redirect to="/" /> : <LoginForm />)} />
+          <Route path="/register" render={() => (isAuthenticated ? <Redirect to="/" /> : <RegisterForm />)} />
+          <Route path="/create" render={() => (isAuthenticated ? <PostForm /> : <Redirect to="/login" />)} />
         </Switch>
       </div>
     </Router>
